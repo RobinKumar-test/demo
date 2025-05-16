db2f7620f0d20b507f08080c06c63a77efd68c5a
Robin Kumar <robinkumar@Robins-MacBook-Pro.local>
2025-05-16 07:17:30 +0100

    initial code

---
+++ CODING/Day 1/F-01.py
@@ -0,0 +1,13 @@
+def calculate_area_of_rectangle(length, width):
+    """
+    Calculate the area of a rectangle.
+    :param length: Length of the rectangle
+    :param width: Width of the rectangle
+    :return: Area of the rectangle
+    """
+    return length * width
+
+
+
+
+        
